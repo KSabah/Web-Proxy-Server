76345d685e4478375743c480081df40e4f4ff18a
Ksabah <khans3@tcd.ie>
2019-02-20 13:42:19 +0000

    Client and server working with threads

--- client.c
+++ client.c
@@ -1,57 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define MAX_REQUEST 8192
+#define PORT 8080 
+#define SA struct sockaddr
 
-void error(const char *msg)
+struct args
 {
-    perror(msg);
-    exit(0);
-}
+	int id;
+	int connfd;
+	char buffer[MAX_REQUEST];
+};
 
-int main(int argc, char *argv[])
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n;  
+    bzero(buff, sizeof(buff)); 
+    printf("Enter request: "); 
+    n = 0; 
+    while ((buff[n++] = getchar()) != '\n') 
+        ; 
+    write(sockfd, buff, sizeof(buff)); 
+    bzero(buff, sizeof(buff)); 
+    read(sockfd, buff, sizeof(buff)); 
+    printf("From Server : %s", buff); 
+    if ((strncmp(buff, "exit", 4)) == 0) { 
+        printf("Client Exit...\n"); 
+    }
+}
+  
+int main(int argc, char** argv) 
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    int port = PORT;	
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli;
+   printf("%d\n", port); 
+    if(argv[1] != NULL) {port = atoi(argv[1]);}
+   printf("%d", port);
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
-}
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_port = htons(port); 
+  
+    // connect the client socket to server socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd); 
+} 
--- server.c
+++ server.c
@@ -1,55 +1,151 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/*
+ From: https://github.com/shawnzam/webproxy-assignment-
+ */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "mysocket.h"
+#include <time.h>
+#include <netdb.h>
+#include <string.h>
 
-void error(const char *msg)
+#define MAX_REQUEST 8192
+#define MAX_URL 2048
+#define WEBPORT 80
+/*
+ This struct contains the info that is passed to a thread.
+ */
+struct args 
 {
-    perror(msg);
-    exit(1);
+    int id;
+    int connfd;
+    char buffer[MAX_REQUEST];
+};
+/*
+ This struct is filled from data from the remote http request. It contains the whole remote request, the method, path, 
+ version, contenttype(not implemented yet), the host and page or file.
+ */
+struct req
+{
+    char request[MAX_REQUEST];
+    char method[16];
+    char path[MAX_URL];
+    char version[16];
+    char contenttype[128];
+    char host[MAX_URL];
+    char page[MAX_URL];
+};
+
+void *handleConnection(void *args);
+void getReqInfo(char *request, struct req *myreq);
+void returnFile(int socket, char* filepath);
+void sendRemoteReq(char filename[MAX_URL], char host[MAX_URL], int socket);
+FILE *fp_log; //log
+
+/*
+ Main setup the socket for listening and enters an infinate while loop. 
+ When a connection arrives it spawns a thread and passes the request to the handleConnection function
+ */
+int main(int argc, char** argv) {
+    time_t result;
+    result = time(NULL);
+    struct tm* brokentime = localtime(&result);
+    int port = atoi(argv[1]);
+    int srvfd  = makeListener(port);
+    int threadID = 0;
+    fp_log = fopen("server.log", "a");
+    fprintf(fp_log, "Server started at %s \nListening on Port %d\n",asctime(brokentime), port);	
+    printf("Server started at %s \nListening on Port %d\n",asctime(brokentime), port);	
+    fclose(fp_log);
+    while (1)
+    {
+        struct args* myargs;
+        myargs = (struct args*) malloc(sizeof(struct args));
+        myargs->connfd = listenFor(srvfd);
+        fp_log = fopen("server.log", "a");
+        pthread_t thread;
+        threadID++;
+        myargs->id = threadID;
+        pthread_create(&thread, NULL, handleConnection, myargs); //thread never joins
+        fprintf(fp_log, "Thread %d created\n", myargs->id); 
+        printf("Thread %d created\n", myargs->id);
+    }
+    printf("Connection closed");
+    close(srvfd);     
+    return 0;
 }
-
-int main(int argc, char *argv[])
+/*
+ Handels the connection by passing the request to the getReqInfo function for parsing 
+ and then retuns the request http page via the sendRemoteReq() function.
+ */
+void *handleConnection(void *args)
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+    struct args* myarg = (struct args*) args;
+    struct req* myreq;
+    myreq = (struct req*) malloc(sizeof(struct req));
+    int bytesRead;
+    bzero(myarg->buffer, MAX_REQUEST);
+    bytesRead = read(myarg->connfd, myarg->buffer, sizeof(myarg->buffer));
+    getReqInfo(myarg->buffer, myreq);
+    sendRemoteReq(myreq->page, myreq->host, myarg->connfd);
+    fprintf(fp_log, "Thread %d exits\n", myarg->id); 
+    printf("Thread %d exits\n", myarg->id);
+    pthread_exit(NULL);
+}
+/*
+ parses the HTTP req and fills the struct req
+ */
+void getReqInfo(char *request, struct req *myreq)
+{
+    char host[MAX_URL], page[MAX_URL];
+    strncpy(myreq->request, request, MAX_REQUEST-1);
+    strncpy(myreq->method, strtok(request, " "), 16-1);
+    strncpy(myreq->path, strtok(NULL, " "), MAX_URL-1);
+    strncpy(myreq->version, strtok(NULL, "\r\n"), 16-1);
+    sscanf(myreq->path, "http://%99[^/]%99[^\n]", host, page);
+    strncpy(myreq->host, host, MAX_URL-1);
+    strncpy(myreq->page, page, MAX_URL-1);
+    fprintf (fp_log, "method: %s\nversion: %s\npath: %s\nhost: %s\npage: %s\n", myreq->method, myreq->version, myreq->path, myreq->host, myreq->page);
+    printf("method: %s\nversion: %s\npath: %s\nhost: %s\npage: %s\n", myreq->method, myreq->version, myreq->path, myreq->host, myreq->page);
+    
+}
+/*
+ Sends the HTTP request to the remote site and retuns the HTTP payload to the connected client.
+ */
+void sendRemoteReq(char filename[MAX_URL], char host[MAX_URL], int socket)
+{
+    time_t result;
+    result = time(NULL);
+    struct tm* brokentime = localtime(&result);
+    int chunkRead;
+    int chunkWritten;
+	size_t fd;
+    char data[512];
+	char reqBuffer[512];
+    bzero(reqBuffer, 512);
+    fd = connectTo(host, WEBPORT);
+    strcat(reqBuffer, "GET ");
+    strcat(reqBuffer, filename);
+    strcat(reqBuffer, " HTTP/1.1\n");
+    strcat(reqBuffer, "host: ");
+    strcat(reqBuffer, host);
+    strcat(reqBuffer, "\n\n");
+    fprintf(fp_log, "request sent to %s :\n%s\nSent at: %s\n", host, reqBuffer, asctime(brokentime));
+    printf("request sent to %s :\n%s\nSent at: %s\n", host, reqBuffer, asctime(brokentime));
+    chunkRead = write(fd, reqBuffer, strlen(reqBuffer));
+    int totalBytesRead = 0;
+    int totalBytesWritten = 0;
+    while ((chunkRead = read(fd, data, sizeof(data)))!= (size_t)NULL)//send file
+    {
+        chunkWritten = write(socket, data, chunkRead);
+        totalBytesRead += chunkRead;
+        totalBytesWritten += chunkWritten;
+    }
+    fprintf (fp_log, "completed sending %s at %d bytes at %s\n------------------------------------------------------------------------------------\n", filename, totalBytesRead, asctime(brokentime));
+    printf("completed sending %s at %d bytes at %s\n------------------------------------------------------------------------------------\n", filename, totalBytesRead, asctime(brokentime));
+    fclose(fp_log);
+    close(fd);
+    close(socket);
 }
